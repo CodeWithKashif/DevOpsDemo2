30772725a633540f419956250bf1ae2adba570bf
kreza <kashif.reza@icreon.com>
2019-06-15 17:23:48 +0530

    Added test project

---
+++ ConsoleAppDevOps.UnitTest/CustomerServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsoleAppDevOps.Domain;
+using NUnit.Framework;
+
+namespace ConsoleAppDevOps.UnitTest
+{
+    [TestFixture]
+    public class CustomerServiceTest
+    {
+        [Test]
+        public void CallGetCustomerName_ShouldReturnByDomain()
+        {
+            var customerService = new CustomerService();
+            Assert.That(customerService.GetCustomerName().Contains("This is from domain"));
+        }
+
+        [Test]
+        public void CallGetCustomerName_ShouldReturnByRepository()
+        {
+            var customerService = new CustomerService();
+            Assert.That(customerService.GetCustomerNameFromRepo().Contains("This is from repository"));
+        }
+    }
+}
---
+++ ConsoleAppDevOps.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleAppDevOps.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppDevOps.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("681d0869-1f09-43a1-9193-ca90840cde9a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
